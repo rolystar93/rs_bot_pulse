e591899922be697d7a147df87aea5a6e597c15c3
Roly <rolystar93@gmail.com>
2026-05-26 11:40:41 -0300

    card para total balance

---
+++ rs_bot_pulse/src/components/CardPortafolioGeneral.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="col-12 col-md-3">
+
+    <q-card class="dashboard-card kpi-card fade-up">
+
+      <div class="pair-title">
+        <div class="pair-icon">
+          <q-icon :name=props.icon size="18px" />
+        </div>
+        <span>
+          {{ props.title }}
+        </span>
+
+
+
+
+      </div>
+
+      <div class="kpi-value">
+        <!-- +$12,480 -->
+        {{ props.average }}
+      </div>
+
+      <div class="kpi-footer">
+
+        <div class="kpi-badge-positive">
+          <!-- +12% -->
+          {{ props.gp }}
+        </div>
+
+        <div class="text-grey">
+          <!-- vs last month -->
+          {{ props.lastmount }}
+        </div>
+
+      </div>
+
+    </q-card>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  title: String,
+  average: String,
+  gp: String,
+  lastmount: String,
+  icon: String
+})
+</script>
--- rs_bot_pulse/src/pages/IndexPage.vue
+++ rs_bot_pulse/src/pages/IndexPage.vue
@@ -10,6 +10,8 @@
         flag1="fi fi-gb pair-flag" flag2="fi fi-us pair-flag" />
       <card-activos-averaje title="USDJPY" average="R$ 12 400" gp="+12%" lastmount="vs last month"
         flag1="fi fi-us pair-flag" flag2="fi fi-jp pair-flag" />
+      <card-portafolio-general title="TOTAL" average="R$ 12 400" gp="+12%" lastmount="vs last month"
+        icon="show_chart" />
     </div>
 
   </q-page>
@@ -17,4 +19,5 @@
 
 <script setup lang="ts">
 import CardActivosAveraje from 'src/components/CardActivosAveraje.vue';
+import CardPortafolioGeneral from 'src/components/CardPortafolioGeneral.vue';
 </script>
