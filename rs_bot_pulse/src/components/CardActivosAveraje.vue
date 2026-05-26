11f1ec312adfbf10cbceab23a7fcef57749f9f0a
Roly <rolystar93@gmail.com>
2026-05-26 10:51:27 -0300

    cards para los resumenes por par

---
+++ rs_bot_pulse/src/components/CardActivosAveraje.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="col-12 col-md-3">
+
+    <q-card class="dashboard-card kpi-card fade-up">
+
+      <div class="pair-title">
+        <div class="pair-flags">
+          <!-- <span class="fi fi-eu pair-flag"></span> -->
+          <span :class=props.flag1></span>
+          <!-- <span class="fi fi-us pair-flag"></span> -->
+          <span :class=props.flag2></span>
+        </div>
+        <span>
+          {{ props.title }}
+        </span>
+
+
+
+
+      </div>
+
+      <div class="kpi-value">
+        <!-- +$12,480 -->
+        {{ props.average }}
+      </div>
+
+      <div class="kpi-footer">
+
+        <div class="kpi-badge-positive">
+          <!-- +12% -->
+          {{ props.gp }}
+        </div>
+
+        <div class="text-grey">
+          <!-- vs last month -->
+          {{ props.lastmount }}
+        </div>
+
+      </div>
+
+    </q-card>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  title: String,
+  average: String,
+  gp: String,
+  lastmount: String,
+  flag1: String,
+  flag2: String
+})
+</script>
--- rs_bot_pulse/src/pages/IndexPage.vue
+++ rs_bot_pulse/src/pages/IndexPage.vue
@@ -3,39 +3,18 @@
 
     <div class="row q-col-gutter-lg">
 
-      <!-- CARD -->
-      <div class="col-12 col-md-3">
-
-        <q-card class="dashboard-card kpi-card fade-up">
-
-          <div class="kpi-title">
-            Total Profit
-          </div>
-
-          <div class="kpi-value">
-            +$12,480
-          </div>
-
-          <div class="kpi-footer">
-
-            <div class="kpi-badge-positive">
-              +12%
-            </div>
-
-            <div class="text-grey">
-              vs last month
-            </div>
-
-          </div>
-
-        </q-card>
-
-      </div>
-
+      <!-- CARDs -->
+      <card-activos-averaje title="EURUSD" average="R$ 12 400" gp="+12%" lastmount="vs last month"
+        flag1="fi fi-eu pair-flag" flag2="fi fi-us pair-flag" />
+      <card-activos-averaje title="GBPUSD" average="R$ 12 400" gp="+12%" lastmount="vs last month"
+        flag1="fi fi-gb pair-flag" flag2="fi fi-us pair-flag" />
+      <card-activos-averaje title="USDJPY" average="R$ 12 400" gp="+12%" lastmount="vs last month"
+        flag1="fi fi-us pair-flag" flag2="fi fi-jp pair-flag" />
     </div>
 
   </q-page>
 </template>
 
 <script setup lang="ts">
+import CardActivosAveraje from 'src/components/CardActivosAveraje.vue';
 </script>
